a78957142044a1ddeaa07fae9da7dd340ecc9118
pedru44 <pedroc@iesb.edu.br>
2024-03-01 21:55:50 -0300

    node

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
 
-app.get('/hello', function () { 
-    
+app.get('/hello', function (req, res) { 
+    res.send('Chegou a resposta')
+})
+app.get('/nome', function (req, res) { 
+    res.send('Pedro')
+})
+app.get('/', function (req, res) { 
+    res.send('Rota Principal')
 })
 
 app.listen(3000, function () {
