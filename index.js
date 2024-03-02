f524681bf27ce852cba2f59e5c56b09112721660
pedru44 <pedroc@iesb.edu.br>
2024-03-01 21:48:34 -0300

    java script

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+app.get('/hello', function () { 
+    
+})
+
+app.listen(3000, function () {
+    console.log('Server Up port 3000')
+})
+
