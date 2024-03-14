5d766293cebc773d7e2fb6d02e1b13b0f5d9ce74
pedru44 <pedroc@iesb.edu.br>
2024-03-13 22:02:47 -0300

    ATUALIZAÇÃO

---
+++ routes/lista4.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const router = express.Router()
+
+router.post('/ex1', function (req, res) { 
+
+    res.json(req.body)
+    
+
+    const numeros = req.body.notas
+
+    /* for (i=0; i<numeros.length; i++){
+        console.log(numeros[i]);
+    } */
+
+    /* for(let n in numeros){
+        console.log('Valor de n é: ->' + n );
+    }
+ */
+    let soma = 0
+    for(let n of numeros){
+         soma += n  
+
+    }
+
+    const media = soma / numeros.length
+
+    res.json({media})
+})
+
+router.post('/ex8', function (req, res) { 
+
+    let soma = 0
+    for(let n of numeros){
+        if(n<0){
+            soma++
+        }
+
+    res.json({soma})
+})
+
+module.exports = router
