8fbfd509a353ecfca246dff894353fc9d86262b0
pedru44 <pedroc@iesb.edu.br>
2024-03-18 20:08:12 -0300

    lista 3

--- routes/lista3.js
+++ routes/lista3.js
@@ -1,6 +1,56 @@
 const express = require('express')
 const router = express.Router()
 
+router.post('/ex1', function (req, res) {
+
+    const salario = req.body.salario
+    let salarioFinal = {}
+
+    if (salario > 2000) {
+        salarioFinal = Number(salario) + (Number(salario) * 50 / 100)
+    } else {
+        salarioFinal = Number(salario) + (Number(salario) * 30 / 100)
+    }
+
+    res.json({ salarioFinal })
+
+})
+
+router.post('/ex2', function (req, res) {
+
+    const { numero1, numero2, numero3 } = req.body
+
+    let maior = numero1
+
+    if (numero2 > maior) {
+        maior = numero2
+    }
+
+    if (numero3 > maior) {
+        maior = numero3
+    }
+
+    const mensagem = `O maior número é ${maior}`
+
+    res.json({ mensagem })
+})
+
+router.post('/ex3', function (req, res) {
+
+    const { pessoas, chopp, pizza, cobertura } = req.body
+
+    const valorChopp = chopp * 4.80;
+    const valorPizza = pizza * 17.00;
+    const valorCobertura = cobertura * 1.50;
+
+    const total = (Number(valorChopp) + Number(valorPizza) + Number(valorCobertura));
+    const final = Number(total) + (Number(total) * 10 / 100)
+
+    const totalPorPessoa = final / pessoas
+
+    res.json({ totalPorPessoa })
+})
+
 router.post('/ex4', function (req, res) {
 
     const { salarioMinimo, horasTrabalhadas, dependentes, horasExtras } = req.body
@@ -23,8 +73,8 @@ router.post('/ex4', function (req, res) {
     const gratificacao = salarioLiquido < 3500 ? 1000 : 50
     const salarioReceber = salarioLiquido + gratificacao
 
-    res.json({salarioBruto , imposto, gratificacao, salarioReceber})
-   
+    res.json({ salarioBruto, imposto, gratificacao, salarioReceber })
+
 })
 
 module.exports = router
--- routes/routes.js
+++ routes/routes.js
@@ -1,16 +1,16 @@
-const express =  require('express')
+const express = require('express')
 const router = express.Router()
 
-const introducao = require('./routes/introducao')
-const lista1 = require('./routes/lista1')
-const lista2 = require('./routes/lista2')
-const lista3 = require('./routes/lista3')
-const lista4 = require('./routes/lista4')
+const introducao = require('./introducao')
+const lista1 = require('./lista1')
+const lista2 = require('./lista2')
+const lista3 = require('./lista3')
+const lista4 = require('./lista4')
 
-app.use('/', introducao)
-app.use('/lista1', lista1)
-app.use('/lista2', lista2)
-app.use('/lista3', lista3)
-app.use('/lista4', lista4)
+router.use('/', introducao)
+router.use('/lista1', lista1)
+router.use('/lista2', lista2)
+router.use('/lista3', lista3)
+router.use('/lista4', lista4)
 
 module.exports = router
