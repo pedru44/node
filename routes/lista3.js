f16b7d41bca4a49777f8dd2c3b74cd60dfd37e0d
pedru44 <pedroc@iesb.edu.br>
2024-03-25 21:28:22 -0300

    atualização

--- routes/lista3.js
+++ routes/lista3.js
@@ -79,18 +79,18 @@ router.post('/ex4', function (req, res) {
 
 router.post('/ex5', function (req, res) {
 
-    const { idAluno, nota1, nota2, nota3, mediaQercicios } = req.body;
+    const { idAluno, n1, n2, n3, mediaDosExercicios } = req.body;
 
-    const mediaAproveitamento = (nota1 + nota2 * 2 + nota3 * 3 + mediaQercicios) / 7;
+    const mediaDeAproveitamento = (n1 + n2 * 2 + n3 * 3 + mediaDosExercicios) / 7;
 
     let conceito;
-    if (mediaAproveitamento >= 9.0) {
+    if (mediaDeAproveitamento >= 9.0) {
         conceito = 'A';
-    } else if (mediaAproveitamento >= 7.5) {
+    } else if (mediaDeAproveitamento >= 7.5) {
         conceito = 'B';
-    } else if (mediaAproveitamento >= 6.0) {
+    } else if (mediaDeAproveitamento >= 6.0) {
         conceito = 'C';
-    } else if (mediaAproveitamento >= 4.0) {
+    } else if (mediaDeAproveitamento >= 4.0) {
         conceito = 'D';
     } else {
         conceito = 'E';
@@ -103,9 +103,46 @@ router.post('/ex5', function (req, res) {
         mensagem = 'Reprovado!';
     }
 
-    res.json({idAluno, nota1, nota2, nota3,mediaQercicios, mediaAproveitamento,conceito, mensagem
-    })
+    res.json({ idAluno, n1, n2, n3, mediaDosExercicios, mediaDeAproveitamento, conceito, mensagem })
 })
+
+router.post('/ex6', function (req, res) {
+
+    const { altura, sexo } = req.body;
+
+    let pesoIdeal;
+
+    if (sexo.toLowerCase() === "masculino") {
+        pesoIdeal = (72.7 * altura) - 58;
+    } else if (sexo.toLowerCase() === "feminino") {
+        pesoIdeal = (62.1 * altura) - 44.7;
+    } else {
+        return res.status(400).json({ error: 'Sexo inválido. Por favor, insira "masculino" ou "feminino". ' });
+    }
+    res.json({ pesoIdeal })
+})
+
+router.post('/ex7', function (req, res) {
+
+    const { v1, v2, v3 } = req.body;
+
+    const n1 = Number(v1);
+    const n2 = Number(v2);
+    const n3 = Number(v3);
+
+    let somaDosMaiores;
+
+    if (n1 > n2 && n1 > n3) {
+        somaDosMaiores = n1 + Math.max(n2, n3);
+    } else if (n2 > n1 && n2 > n3) {
+        somaDosMaiores = n2 + Math.max(n1, n3);
+    } else {
+        somaDosMaiores = n3 + Math.max(n1, n2);
+    }
+
+    res.json({ somaDosMaiores })
+})
+
 
 
 module.exports = router
